79f3491572235df5d8e6bbb59eafacb04a75d6e2
zz871997 <zz15121998@gmail.com>
2017-11-02 19:53:45 +0700

    Add Write to Excel

--- Library-ProjectI/src/tranquangkhai20152005/library/controller/ThongKeController.java
+++ Library-ProjectI/src/tranquangkhai20152005/library/controller/ThongKeController.java
@@ -2,13 +2,30 @@ package tranquangkhai20152005.library.controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
+
+import org.apache.poi.ss.usermodel.BorderStyle;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import tranquangkhai20152005.library.model.BookDB;
 import tranquangkhai20152005.library.view.MainUI;
@@ -49,6 +66,7 @@ public class ThongKeController {
 					thongKeView = new ThongKeView(mainUI);
 					thongKeInformation = thongKeView.getThongKeInformation();
 					thongKeView.setVisible(true);
+					
 					DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
 					LocalDate localDate   = LocalDate.now();
 					String ngayHT     = Integer.toString(localDate.getDayOfMonth());
@@ -61,14 +79,22 @@ public class ThongKeController {
 					String title = cbTKSach.getSelectedItem().toString();
 					thongKeInformation.getLbTitle().setText("THỐNG KÊ SÁCH THEO " + title);
 					
-					loadDataForTKTable(indexOfCombo);
+					
+					//System.out.println("Index cb: " + indexOfCombo);
+					String data[][] = getDataTKSachFromDB(indexOfCombo);
+
+					thongKeInformation.getTableThongKeView().updateTable(data, indexOfCombo);
+					//loadDataForTKTable(indexOfCombo);
+					
+					setActionForThongKeSachView(data);
+					
 				
 				}
 			}
 		});
 	}
 	
-	private void loadDataForTKTable(int indexOfCb) {
+	private String[][] getDataTKSachFromDB(int indexOfCb) {
 		ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
 		if (indexOfCb == 1) {
 			result = bookDB.thongKeSach("Tacgia");
@@ -85,7 +111,8 @@ public class ThongKeController {
 		}
 		
 		String[][] data = convertToString(result);
-		thongKeInformation.getTableThongKeView().updateTable(data, indexOfCb);
+		return data;
+		
 	}
 	
 	/* Convert ArrayList<ArrayList<String>> to String[][] */
@@ -100,21 +127,191 @@ public class ThongKeController {
 		return convertResult;
 	}
 	
+	/* Set Action for thongKeSachView - Print & Cancel */
+	private void setActionForThongKeSachView(String[][] data) {
+		JButton btnPrint  = thongKeView.getBtnPrint();
+		JButton btnCancel = thongKeView.getBtnCancel();
+		
+		btnPrint.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				inThongKe(data);
+			}
+		});
+		
+		btnCancel.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				huy();
+			}
+		});		
+	}
 	
+	/* In Thong ke */
+	private void inThongKe(String[][] data) {
+		JFileChooser fileChooser = new JFileChooser();
+		int select = fileChooser.showSaveDialog(this.thongKeView);
+		String saveFilePath = "";
+		
+		if (select == JFileChooser.APPROVE_OPTION) {
+			String path = fileChooser.getCurrentDirectory().toString() 
+			       	   + "\\" + fileChooser.getSelectedFile().getName();
+			if(path.indexOf(".xlsx") >= 0) {
+				saveFilePath = path;
+			}
+			else {
+				saveFilePath = path + ".xlsx";
+			}
+			
+			System.out.println("Save file to: " + saveFilePath);
+			saveThongKeExcelTo(saveFilePath, data);
+		}
+	}
 	
+	private void saveThongKeExcelTo(String path, String data[][]) {
+		try {
+			FileOutputStream fos = new FileOutputStream(new File(path));
+			XSSFWorkbook workbook = new XSSFWorkbook();
+			XSSFSheet sheet = workbook.createSheet("ThongKeSach");
+			sheet.setColumnWidth(0, 8200);
+			sheet.setColumnWidth(1, 6000);
+			sheet.setColumnWidth(2, 8200);
+			
+			Row row = sheet.createRow(0);
+			Cell cell = row.createCell(0, CellType.STRING);
+			cell.setCellValue("TRƯỜNG ĐẠI HỌC BÁCH KHOA HÀ NỘI");
+			cell.setCellStyle(createStyleDefault(workbook));
+			cell = row.createCell(2, CellType.STRING);
+			cell.setCellValue("Cộng hòa xã hội chủ nghĩa Việt Nam");
+			cell.setCellStyle(createStyleDefault(workbook));
+			
+			row = sheet.createRow(1);
+			cell = row.createCell(0, CellType.STRING);
+			cell.setCellValue("Thư viện Tạ Quang Bửu");
+			cell.setCellStyle(createStyleDefault(workbook));
+			cell = row.createCell(2, CellType.STRING);
+			cell.setCellValue("Độc lập - Tự do - Hạnh phúc");
+			cell.setCellStyle(createStyleDefault(workbook));
+			
+			row = sheet.createRow(2);
+			cell = row.createCell(0, CellType.STRING);
+			cell.setCellValue("Trần Quang Khải - 20152005");
+			cell.setCellStyle(createStyleDefault(workbook));
+			
+			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+			LocalDate localDate   = LocalDate.now();
+			String ngayHT     = Integer.toString(localDate.getDayOfMonth());
+			String thangHT    = Integer.toString(localDate.getMonthValue());
+			String namHT      = Integer.toString(localDate.getYear());
+			String now = "Ngày " + ngayHT + "-" + thangHT + "-" + namHT;
+			row = sheet.createRow(3);
+			cell = row.createCell(2, CellType.STRING);
+			cell.setCellValue(now);
+			cell.setCellStyle(createStyleForDate(workbook));
+			
+			row = sheet.createRow(5);
+			cell = row.createCell(0, CellType.STRING);
+			cell.setCellValue(thongKeInformation.getLbTitle().getText().toString().toUpperCase());
+			cell.setCellStyle(createStyleForTitle(workbook));
+			sheet.addMergedRegion(new CellRangeAddress(5, 5, 0, 2));
+			
+			row = sheet.createRow(7);
+			cell = row.createCell(0, CellType.STRING);
+			cell.setCellValue("STT");
+			cell.setCellStyle(createStyleForTableTitle(workbook));
+			cell = row.createCell(1, CellType.STRING);
+			cell.setCellValue(cbTKSach.getSelectedItem().toString().toUpperCase());
+			cell.setCellStyle(createStyleForTableTitle(workbook));
+			cell = row.createCell(2, CellType.STRING);
+			cell.setCellValue("SỐ LƯỢNG");
+			cell.setCellStyle(createStyleForTableTitle(workbook));
+			
+			int rowNum = 8;
+			for (int i = 0; i < data.length; i++) {
+				row = sheet.createRow(rowNum);
+				for (int j = 0; j < 3; j++) {
+					cell = row.createCell(j, CellType.STRING);
+					cell.setCellValue(data[i][j]);
+					cell.setCellStyle(createStyleDataTable(workbook));
+				}
+				rowNum++;
+			}
+			
+			row = sheet.createRow(rowNum + 2);
+			cell = row.createCell(0, CellType.STRING);
+			cell.setCellValue("Người lập");
+			cell.setCellStyle(createStyleDefault(workbook));
+			cell = row.createCell(2, CellType.STRING);
+			cell.setCellValue("Chữ kí thủ thư");
+			cell.setCellStyle(createStyleDefault(workbook));
+			
+			row = sheet.createRow(rowNum + 3);
+			cell = row.createCell(2, CellType.STRING);
+			cell.setCellValue("Admin");
+			cell.setCellStyle(createStyleDefault(workbook));
+			
+			workbook.write(fos);
+			System.out.println("Create file: " + path);
+			fos.close();
+		} 
+		catch (IOException e) {
+			JOptionPane.showMessageDialog(new JDialog(), "Lỗi File");
+			e.printStackTrace();
+		}
+	}
 	
+	private XSSFCellStyle createStyleForDate (XSSFWorkbook workbook) {
+		XSSFCellStyle styleDate = workbook.createCellStyle();
+		XSSFFont font = workbook.createFont();
+		font.setItalic(true);
+		styleDate.setFont(font);
+		styleDate.setAlignment(HorizontalAlignment.CENTER);
+		return styleDate;
+	}
 	
+	private XSSFCellStyle createStyleForTitle (XSSFWorkbook workbook) {
+		XSSFCellStyle styleTitle = workbook.createCellStyle();
+		XSSFFont font = workbook.createFont();
+		font.setBold(true);
+		font.setColor(IndexedColors.BLUE.index);
+		font.setFontHeight(18);
+		styleTitle.setFont(font);
+		styleTitle.setAlignment(HorizontalAlignment.CENTER);
+		return styleTitle;
+	}
 	
+	private XSSFCellStyle createStyleForTableTitle (XSSFWorkbook workbook) {
+		XSSFCellStyle styleTableTitle = workbook.createCellStyle();
+		XSSFFont font = workbook.createFont();
+		font.setBold(true);
+		font.setFontHeight(12);
+		styleTableTitle.setFont(font);
+		styleTableTitle.setAlignment(HorizontalAlignment.CENTER);
+		styleTableTitle.setAlignment(HorizontalAlignment.CENTER);
+		styleTableTitle.setBorderBottom(BorderStyle.MEDIUM);
+		styleTableTitle.setBorderTop(BorderStyle.MEDIUM);
+		styleTableTitle.setBorderRight(BorderStyle.MEDIUM);
+		styleTableTitle.setBorderLeft(BorderStyle.MEDIUM);
+		return styleTableTitle;
+	}
 	
+	private XSSFCellStyle createStyleDefault(XSSFWorkbook workbook) {
+		XSSFCellStyle styleDefault = workbook.createCellStyle();
+		styleDefault.setAlignment(HorizontalAlignment.CENTER);
+		return styleDefault;
+	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	private XSSFCellStyle createStyleDataTable(XSSFWorkbook workbook) {
+		XSSFCellStyle styleData = workbook.createCellStyle();
+		styleData.setAlignment(HorizontalAlignment.CENTER);
+		styleData.setBorderBottom(BorderStyle.MEDIUM);
+		styleData.setBorderTop(BorderStyle.MEDIUM);
+		styleData.setBorderRight(BorderStyle.MEDIUM);
+		styleData.setBorderLeft(BorderStyle.MEDIUM);
+		return styleData;
+	}
+
+	private void huy() {
+		this.thongKeView.setVisible(false);
+	}
 }
